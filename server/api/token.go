ab0f94063d4fe467ab01bef323531863676b2ba7
Oliver <oliver@LM4.eu>
2026-01-02 02:12:27 +0100

    Untitle error message

--- server/api/token.go
+++ server/api/token.go
@@ -135,7 +135,7 @@ func genAccessToken(w http.ResponseWriter, r *http.Request) (accessToken, timeou
 	// verify the refresh token
 	token, err := jwt.ParseWithClaims(m.RefreshToken, &tokens.RefreshClaims{}, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(ns.RefreshKey), nil
 	})
